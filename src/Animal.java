06d295a4b51f314a5546832175adfb0553d84a45
barsukovpavell <barsukovpavell@gmail.com>
2025-06-08 23:09:57 +0300

    Add classes Main and Animal

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    String name;
+
+    public void eat(){
+        System.out.println("Я умею есть.");
+    }
+
+    public void sleep(){
+        System.out.println("Я умею спать.");
+    }
+}
